acaa91b252366280fc323bc83995c0307068f9ab
Pavnesh12 <pavneshprajapati898@gmail.com>
2021-12-01 21:30:37 +0530

    percentage of getting head and tail

---
+++ Basicprogramm/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basicprogramm
+{
+    public class Coin
+    {
+        double head = 0;
+        double tail = 0;
+     
+       public Coin(int number)
+        {
+ 
+            Random random = new Random();
+            for (int i = 0; i<number; i++)
+            {
+                int toss = random.Next(0, 2);
+                if (toss == 1)
+                {
+                    head++;
+                }
+                else
+                {
+                    tail++;
+                }
+
+
+            }
+            head = head * 100 / number;
+            tail = tail * 100 / number;
+
+        }
+        public void percentage()
+        {
+            Console.WriteLine("percentage of head is:" + head);
+            Console.WriteLine("percentage of tail is:" + tail);
+        }
+    }
+  
+}
