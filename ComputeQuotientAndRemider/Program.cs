b436b4c72b6ad2c96326dfae1fdcf574528ef938
Pavnesh12 <pavneshprajapati898@gmail.com>
2021-12-02 21:58:53 +0530

    calculate the reminder and quotient

---
+++ ComputeQuotientAndRemider/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ComputeQuotientAndRemider
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the dividend and divisor\n");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            int divisor = Convert.ToInt32(Console.ReadLine()); 
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+            Console.WriteLine("Quotient = " + quotient);
+            Console.WriteLine("Remainder = " + remainder);
+           
+        }
+    }
+}
