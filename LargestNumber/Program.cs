e28dc276c6193e7e3efbd3090d7d952277fc60ab
Pavnesh12 <pavneshprajapati898@gmail.com>
2021-12-04 16:00:46 +0530

    Find the largest number

---
+++ LargestNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+            // set the value of the three numbers
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.Write("Number one is the largest!\n");
+                }
+                else
+                {
+                    Console.Write("Number three is the largest!\n");
+                }
+            }
+            else if (num2 > num3)
+                Console.Write("Number two is the largest!\n");
+            else
+                Console.Write("Number three is the largest!\n");
+        }
+    }
+    
+}
